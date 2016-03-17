d381aa4f7a5b049ee9ab2a8e7daacdeff5d8ecdf
Andrey Ermolenko <andrey.ermolenko@xored.com>
2016-03-17 19:31:02 +0600

    Fix getAuthGameMode() for UE

--- modules/world.nim
+++ modules/world.nim
@@ -1,6 +1,5 @@
 # Copyright 2016 Xored Software, Inc.
 
 class(UWorld of UObject, header: "Engine/World.h", notypedef):
-  proc getNavigationSystem(): ptr UNavigationSystem
-  proc getAuthGameMode[T](): ptr T
-  proc getAuthGameMode(): ptr AGameMode
+  proc getNavigationSystem*(): ptr UNavigationSystem
+  proc getAuthGameMode*(): ptr AGameMode
