fc6510011251f2e21cdf496078286be8bb937b75
Andrey Ermolenko <andrey.ermolenko@xored.com>
2016-04-25 01:19:07 +0600

    Added template to create TArray of uenum

--- modules/containers/enumasbyte.nim
+++ modules/containers/enumasbyte.nim
@@ -1,11 +1,12 @@
 # Copyright 2016 Xored Software, Inc.
 
-class(TEnumAsByte[T: enum], header: "Container/EnumAsByte.h", bycopy):
-  proc makeTEnumAsByte(e: T): TEnumAsByte[T] {.constructor.}
+class(TEnumAsByte[T: enum], header: "Containers/EnumAsByte.h", bycopy):
   proc value(): T {.noSideEffect.}
+
+proc initTEnumAsByte*[T: enum](e: T): TEnumAsByte[T] {.importcpp: "TEnumAsByte<'1>(@)", nodecl, constructor.}
 
 converter toEnumValue*[T](container: TEnumAsByte[T]): T =
   result = container.value()
 
 converter fromEnumValue*[T: enum](t: T): TEnumAsByte[T] =
-  result = makeTEnumAsByte(t)
+  result = initTEnumAsByte(t)
