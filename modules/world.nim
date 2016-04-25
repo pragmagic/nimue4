2f06140a2321a1fb384ee4655bd6b55a414c907b
Andrey Ermolenko <andrey.ermolenko@xored.com>
2016-04-26 03:02:25 +0600

    Added function to get player controller from world

--- modules/world.nim
+++ modules/world.nim
@@ -3,3 +3,4 @@
 class(UWorld of UObject, header: "Engine/World.h", notypedef):
   proc getNavigationSystem*(): ptr UNavigationSystem
   proc getAuthGameMode*(): ptr AGameMode
+  proc getFirstPlayerController*(): ptr APlayerController
