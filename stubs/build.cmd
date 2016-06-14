68aec3ab7f106c6314529cd6192a2ee85af92397
Ruslan Mustakov <ruslan.mustakov@xored.com>
2016-06-14 13:14:43 +0600

    Modified stubs to point to UE 4.12

--- stubs/build.cmd
+++ stubs/build.cmd
@@ -3,7 +3,7 @@
 set NIM_HOME=C:\Program Files (x86)\Nim\ rem change this
 set NIMUE_HOME=C:\Users\jdoe\nimue4\ rem change this
 set VS_HOME=C:\Program Files (x86)\Microsoft Visual Studio 14.0
-set UE4_HOME=C:\Program Files (x86)\Epic Games\4.11
+set UE4_HOME=C:\Program Files (x86)\Epic Games\4.12
 
 set PATH=%PATH%;%NIM_HOME%\bin\
 
--- stubs/build.sh
+++ stubs/build.sh
@@ -2,6 +2,6 @@
 
 export NIMUE_HOME=/Path/To/nimue4/ # change this
 export NIM_HOME=/Path/To/Nim/ # change this
-export UE4_HOME=/Users/Shared/UnrealEngine/4.11/
+export UE4_HOME=/Users/Shared/UnrealEngine/4.12/
 
 nim "$@"
