883b0ec0bb6cab777ce3a4578b26395dbfbcaaf6
Konstantin Zaitcev <kosz.storage@gmail.com>
2016-08-17 12:00:13 +0700

    Added base methods for USkeletalMeshComponent (#6)

--- modules/components/skeletalmesh.nim
+++ modules/components/skeletalmesh.nim
@@ -1,7 +1,18 @@
 # Copyright 2016 Xored Software, Inc.
 
-wclass(USkeletalMeshComponent, header: "Components/SkeletalMeshComponent.h", notypedef):
+wclass(USkeletalMeshComponent of USkinnedMeshComponent, header: "Components/SkeletalMeshComponent.h", notypedef):
   var bounds: FBoxSphereBounds
   var animScriptInstance: ptr UAnimInstance
+
+  method setSkeletalMesh(newMesh: ptr USkeletalMesh, bReinitPose:bool)
+    ## Change the SkeletalMesh that is rendered for this Component. Will re-initialize the animation tree etc.
+    ## @param newMesh - New mesh to set for this component
+    ## @param bReinitPose - Whether we should keep current pose or reinitialize.
+
+  method setAnimation(newAnimToPlay: ptr UAnimationAsset)
+  method playAnimation(newAnimToPlay: ptr UAnimationAsset, bLooping: bool)
+
+  method play(bLooping: bool)
+  method stop()
 
 # TODO
