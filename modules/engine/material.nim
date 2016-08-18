ce0df49046de33c16335c95fb1b4677f06d6c762
Konstantin Zaitcev <kosz.storage@gmail.com>
2016-08-18 17:21:07 +0700

    Added method to set material parameters (#7)

--- modules/engine/material.nim
+++ modules/engine/material.nim
@@ -2,6 +2,10 @@
 
 wclass(UMaterialInstanceDynamic of UMaterialInstance, header: "Materials/MaterialInstanceDynamic.h", notypedef):
 
-  proc copyParameterOverrides(materialInstance: ptr UMaterialInstance) {.importcpp.}
+  proc copyParameterOverrides(materialInstance: ptr UMaterialInstance)
     ## Copy parameter values from another material instance. This will copy only parameters explicitly overridden in that material instance!!
     ## @param materialInstance			material instance to copy parameters.
+
+  proc setScalarParameterValue(parameterName: FName, value: float32)
+  proc setTextureParameterValue(parameterName: FName, value: ptr UTexture)
+  proc setVectorParameterValue(parameterName: FName, value: FLinearColor)
