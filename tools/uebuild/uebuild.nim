600a910b904edafda42be15fce716eeabbd5dcce
Ruslan Mustakov <r.mustakov@gmail.com>
2017-02-14 11:32:45 +0700

    Link openssl statically on iOS/Android

--- tools/uebuild/uebuild.nim
+++ tools/uebuild/uebuild.nim
@@ -290,6 +290,9 @@ proc createNimCfg(outDir: string; moduleDir, nimcacheDir, rootFile: string;
   if platform == "android":
     contents.add("--define:android\n")
     contents.add("--define:android4\n")
+  if platform == "ios" or platform == "android":
+    contents.add("--dynlibOverride:ssl\n")
+    contents.add("--dynlibOverride:crypto\n")
   if isEditorBuild:
     contents.add("--define:editor\n")
   contents.add("--path:\"" & moduleDir.replace("\\", "/") & "\"\n")
