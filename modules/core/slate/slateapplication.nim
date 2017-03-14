cbe276c4ca4bd145f5507c7e595cc016b1115cfc
Ruslan Mustakov <r.mustakov@gmail.com>
2017-03-14 11:56:13 +0700

    Wrap DragTriggerDistance methods

--- modules/core/slate/slateapplication.nim
+++ modules/core/slate/slateapplication.nim
@@ -4,6 +4,12 @@ wclass(FSlateApplication, header: "Framework/Application/SlateApplication.h", by
   proc setAllUserFocusToGameViewport(reasonFocusIsChanging = EFocusCause.SetDirectly)
     ## Sets all users focus to the SWidget representing the currently active game viewport
 
+  proc getDragTriggerDistance(): float32 {.noSideEffect.}
+    ## @return the deadzone size for dragging in screen pixels (aka virtual desktop pixels)
+
+  proc setDragTriggerDistance(screenPixels: float32)
+    ## Set the size of the deadzone for dragging in screen pixels
+
 proc getFSlateApplication*(): ptr FSlateApplication {.
   importcpp: "(& FSlateApplication::Get())", header: "Framework/Application/SlateApplication.h".}
   ## Returns the current instance of the application. The application should have been initialized before
