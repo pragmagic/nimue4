9c744ef44f5de7deadb2031aaae8bbcd4a28156a
Evgeny Zuev <evgeny.zuev@xored.com>
2017-06-05 12:20:03 +0700

    Wrap UGameplayStatics::GetGlobalTimeDilation

--- modules/core/kismet/gameplaystatics.nim
+++ modules/core/kismet/gameplaystatics.nim
@@ -2,3 +2,6 @@
 
 proc setGlobalTimeDilation*(worldContextObject: ptr UObject, timeDilation: cfloat) {.
   header: "Kismet/GameplayStatics.h", importcpp:"UGameplayStatics::SetGlobalTimeDilation(@)".}
+  
+proc getGlobalTimeDilation*(worldContextObject: ptr UObject): cfloat {.
+  header: "Kismet/GameplayStatics.h", importcpp:"UGameplayStatics::GetGlobalTimeDilation(@)".}
